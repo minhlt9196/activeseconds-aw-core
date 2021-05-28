a509d4aaeac6ff47175294ad19f537a091b7f93d
minhlt9196 <minhlt9196@gmail.com>
2021-05-28 13:40:22 +0700

    first commit

---
+++ aw_core/__about__.py
@@ -0,0 +1,25 @@
+# Inspired by:
+# https://github.com/pypa/pipfile/blob/master/pipfile/__about__.py
+
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
+
+__title__ = "aw-core"
+__summary__ = "Core library for TimeBench"
+__uri__ = "https://github.com/TimeBench/aw-core"
+
+__version__ = "0.4.1"
+
+__author__ = "Martin Perreo Sola, Simon"
+__email__ = "martin_perreo_sola@gmail.com, simon@gmail.com"
+
+__license__ = "MPL2"
+__copyright__ = "Copyright 2017 %s" % __author__
